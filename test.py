acb45fcaa194e20dc36415669ed4c425d0fc13b7
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2019-11-25 21:06:00 +0100

    Write actual tests using pytest
    2 of them fail currently and need to be fixed later

--- test.py
+++ test.py
@@ -1,7 +1,48 @@
-Maxtour =5
+import pytest
 
-def tour():
-    tu = Maxtour * 5
-    return tu
+import partieA
 
-print(tour())
+
+def test_init_plateau():
+    plateau = partieA.init(3)
+    
+    assert plateau[0] == [3, 2, 1]
+    assert plateau[1] == plateau[2] == []
+    assert len(plateau) == 3
+
+
+def test_nombre_disques():
+    plateau = [[1, 2], [], [3]]
+
+    assert partieA.nombre_disques(plateau, 0) == 2
+    assert partieA.nombre_disques(plateau, 1) == 0
+    assert partieA.nombre_disques(plateau, 2) == 1
+
+
+def test_disque_superieur():
+    plateau = [[], [3, 2], [4, 1]]
+
+    assert partieA.disque_superieur(plateau, 0) == 0
+    assert partieA.disque_superieur(plateau, 1) == 2
+    assert partieA.disque_superieur(plateau, 2) == 1
+
+
+def test_position_disque():
+    plateau = [[], [1], [3, 2]]
+
+    assert partieA.position_disque(plateau, 1) == (1, 0, 1)
+    assert partieA.position_disque(plateau, 3) == (2, 0, 2)
+    assert partieA.position_disque(plateau, 2) == (2, 1, 2)
+    
+    with pytest.raises(IndexError):
+        partieA.position_disque(plateau, 4)
+
+
+def test_verifier_deplacement():
+    plateau = [[2, 1], [], [3]]
+
+    assert partieA.verifier_deplacement(plateau, 0, 1) == True
+    assert partieA.verifier_deplacement(plateau, 0, 2) == True
+    assert partieA.verifier_deplacement(plateau, 1, 2) == False
+    assert partieA.verifier_deplacement(plateau, 2, 0) == False
+    assert partieA.verifier_deplacement(plateau, 0, 0) == False
