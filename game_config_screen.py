0195a27f566c90db39b92017759fffe97cf34047
IGABBRO <mattdecorsaire@gmail.com>
2019-12-08 12:36:38 +0100

    collect added

--- game_config_screen.py
+++ game_config_screen.py
@@ -9,14 +9,24 @@ class GameConfigScreen(tk.Frame):
 
     def __init__(self, parent, controller):
         super().__init__(parent)
-
+        
         tk.Label(self, text="Nom").pack()
-        input_name = tk.Entry(self)
-        input_name.pack()
+        
+        namevar = tk.StringVar()
+        tk.Entry(self, textvariable=namevar).pack()
 
-        scale_n = tk.Scale(self, orient='horizontal', from_=2, to=20)
-        scale_n.pack()
-
-        button_play = tk.Button(self, text="OK", command=lambda: controller.show_frame(GameScreen))
+        nvar = tk.IntVar()
+        tk.Scale(self, variable = nvar ,orient='horizontal', from_=2, to=20).pack()
+        
+        button_play = tk.Button(self, text="OK", command= lambda:self.collect(nvar, namevar, controller))
         button_play.pack()
+
+    def collect(self, nvar, namevar, controller):
+            self.n = nvar.get()
+            self.name = namevar.get()
+            print(self.n, self.name) 
+            controller.show_frame(GameScreen)
+            
+
+    
 
